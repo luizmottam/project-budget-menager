89b336cf38ef240d1fe6ef6bae035c70444e20f4
Luiz Motta <luizdamm@gmail.com>
2024-06-12 18:57:46 -0300

    Ajustando funções de Banco de Dados

---
+++ db/database.js
@@ -0,0 +1,26 @@
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('database.db');
+
+db.serialize(() => {
+    db.run(`CREATE TABLE IF NOT EXISTS income_distribution (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        salary REAL NOT NULL,
+        tithing_percentage REAL NOT NULL,
+        investment_percentage REAL NOT NULL,
+        education_percentage REAL NOT NULL,
+        savings_percentage REAL NOT NULL,
+        wants_percentage REAL NOT NULL,
+        general_expenses_percentage REAL NOT NULL,
+        created_at DATETIME
+        );`);
+
+    db.run(`CREATE TABLE IF NOT EXISTS expenses (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        category TEXT NOT NULL,
+        amount REAL NOT NULL,
+        description TEXT,
+        date DATETIME
+        );`);
+});
+
+module.exports = db;
